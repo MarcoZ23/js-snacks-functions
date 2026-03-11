9212b307352d72489bbe3bab1f1c3b8a49251d7e
MarcoZ23 <zollomarco23@gmail.com>
2026-03-11 21:29:19 +0100

    complete task 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const aLetter = (array, letter) => {
+const aName = [] 
+for (let i = 0; i < array.length; i++) {
+if (array [i] [0] === letter) {
+aName.push(array[i])}}
+return aName
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const newName = aLetter(names, "A")
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+console.log(newName);
